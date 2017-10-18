85f8c543fec0edcfd91a71e02db7419d322c075a
Cosmin Harangus <cosmin@around25.com>
2017-10-18 12:41:10 +0300

    #1 - Partially added support for script tasks with a NodeJs backend.

--- lib/bpmn/activity/task/script.ex
+++ lib/bpmn/activity/task/script.ex
@@ -1,6 +1,10 @@
 defmodule Bpmn.Activity.Task.Script do
   @moduledoc """
   Handle passing the token through an event element.
+
+    iex> Bpmn.Port.Nodejs.eval_string("1+1", %{cosmin: "soss"})
+    %{"context" => %{"cosmin" => "soss"}, "script" => "1+1", "type" => "string"}
+
   """
 
   @doc """
@@ -13,13 +17,14 @@ defmodule Bpmn.Activity.Task.Script do
   @doc """
   Execute the start event business logic
   """
-  def execute({:bpmn_activity_task_script, {source, target, condition} }, context) do
-      case Bpmn.Expression.execute(condition, context) do
-        {:ok, true} -> execute({:bpmn_sequence_flow, {source, target}}, context)
-        {:ok, false} -> {:false}
-        _ -> {:error}
-      end
-    end
+  def execute({:bpmn_activity_task_script, %{outputs: outputs, type: type, script: script} }, context) do
+    IO.inspect Bpmn.Port.Nodejs.eval_string("1+3", %{testing: "something"})
+#    case Bpmn.Expression.execute(condition, context) do
+#      {:ok, true} -> execute({:bpmn_sequence_flow, {source, target}}, context)
+#      {:ok, false} -> {:false}
+#      _ -> {:error}
+#    end
+  end
   def execute(elem, context), do: tokenOut(elem, context)
 
 end
--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Bpmn.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger],
+     mod: {Bpmn.Application, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +29,8 @@ defmodule Bpmn.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:poison, "~> 3.1"}
+    ]
   end
 end
